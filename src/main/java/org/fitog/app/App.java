9c7e734bc2f14500a2063d4b4a33937e5e7df036
Filippo Tognacci <filippo.tognacci@triodos.com>
2020-09-19 17:24:23 +0200

    first implementation of text extraction

--- src/main/java/org/fitog/app/App.java
+++ src/main/java/org/fitog/app/App.java
@@ -1,12 +1,41 @@
 package org.fitog.app;
 
+import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        PDFParser parser;
+        PDDocument pdDoc = null;
+        PDFTextStripper pdfStripper;
+
+        String parsedText;
+        String fileName = "/Users/ftognacci/sandboxes/Perso/resources/god of carnage script.pdf";
+        File file = new File(fileName);
+        try {
+            parser = new PDFParser(new RandomAccessBufferedFileInputStream(file));
+            parser.parse();
+            pdDoc = parser.getPDDocument();
+            pdfStripper = new PDFTextStripper();
+            parsedText = pdfStripper.getText(pdDoc);
+            System.out.println(parsedText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pdDoc != null)
+                    pdDoc.close();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
     }
 }
