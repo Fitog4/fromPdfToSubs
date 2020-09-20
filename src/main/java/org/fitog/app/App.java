20a41e71f3901f12131f813d85d3e7ee14f84252
Filippo Tognacci <filippo.tognacci@triodos.com>
2020-09-20 14:28:37 +0200

    cleanup string and recognize new replique lines

--- src/main/java/org/fitog/app/App.java
+++ src/main/java/org/fitog/app/App.java
@@ -6,6 +6,9 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Hello world!
@@ -13,11 +16,13 @@ import java.io.File;
  */
 public class App {
     public static void main( String[] args ) {
+
+        // extract text from pdf
         PDFParser parser;
         PDDocument pdDoc = null;
         PDFTextStripper pdfStripper;
 
-        String parsedText;
+        String parsedText = null;
         String fileName = "/Users/ftognacci/sandboxes/Perso/resources/god of carnage script.pdf";
         File file = new File(fileName);
         try {
@@ -26,7 +31,6 @@ public class App {
             pdDoc = parser.getPDDocument();
             pdfStripper = new PDFTextStripper();
             parsedText = pdfStripper.getText(pdDoc);
-            System.out.println(parsedText);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -37,5 +41,29 @@ public class App {
                 e1.printStackTrace();
             }
         }
+
+        // cleanup text
+        List<String> originalLines = Arrays.asList(parsedText.split("\n"));
+        List<String> linesToBePrinted = new ArrayList<>();
+
+        for (int i = 29; i < originalLines.size(); i++) {
+            if (i > 1661) {
+                continue;
+            }
+            if (originalLines.get(i).matches("\\s") || originalLines.get(i).matches("^\\[\\d+\\].*")) {
+                continue;
+            }
+            linesToBePrinted.add(originalLines.get(i));
+        }
+
+        for (int i = 0; i < linesToBePrinted.size(); i++) {
+            String lineToBePrinted = linesToBePrinted.get(i);
+            String newRepliquePointer = isNewReplique(lineToBePrinted) ? "---> " : "";
+            System.out.println(i + "\t" + newRepliquePointer + lineToBePrinted);
+        }
+    }
+
+    private static boolean isNewReplique(String line) {
+        return line.matches("^[A-Z][A-Z][A-Z][A-Z].*");
     }
 }
